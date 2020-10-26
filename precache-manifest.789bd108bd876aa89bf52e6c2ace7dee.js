88cb504eb1b1c22be377b9589e3a6a066801e260
anmolgrwl17 <anmolgrwl17@gmail.com>
2020-10-26 12:20:42 +0530

    Updates

---
+++ precache-manifest.789bd108bd876aa89bf52e6c2ace7dee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69976f2e7ef556099110eb5a275ed95d",
+    "url": "/AirbnbClone/index.html"
+  },
+  {
+    "revision": "2b2981716177143f254d",
+    "url": "/AirbnbClone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "8cf88c76eb295f184602",
+    "url": "/AirbnbClone/static/css/main.86261388.chunk.css"
+  },
+  {
+    "revision": "2b2981716177143f254d",
+    "url": "/AirbnbClone/static/js/2.f17ffc42.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AirbnbClone/static/js/2.f17ffc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf88c76eb295f184602",
+    "url": "/AirbnbClone/static/js/main.77401dd0.chunk.js"
+  },
+  {
+    "revision": "c04f0f0bd25646b09c2a",
+    "url": "/AirbnbClone/static/js/runtime-main.1e26fc81.js"
+  }
+]);
